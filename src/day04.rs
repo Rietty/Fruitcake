202dfaf68fe5bf2e47289e469968d86d867fde0e
Juliette Lavoie <rie.juliette@gmail.com>
2022-12-04 01:23:09 -0500

    Updated Day 04.

--- src/day04.rs
+++ src/day04.rs
@@ -2,25 +2,23 @@
 
 use crate::library::Pair;
 
-pub fn solve(data: &Vec<(Pair, Pair)>) -> (i32, i32) {
-    let mut c1 = 0;
-    let mut c2 = 0;
+pub fn solve(data: &[(Pair, Pair)]) -> (i32, i32) {
+    data.iter()
+        .fold((0, 0), |(mut c1, mut c2), (first, second)| {
+            if (first.x <= second.x && first.y >= second.y)
+                || (second.x <= first.x && second.y >= first.y)
+            {
+                c1 += 1;
+            }
 
-    for line in data {
-        let (one, two) = line;
+            if (first.x <= second.x && first.y >= second.x)
+                || (second.x <= first.x && second.y >= first.x)
+            {
+                c2 += 1;
+            }
 
-        // Count how many ranges are fully enclosed by the other.
-        if (one.x <= two.x && one.y >= two.y) || (two.x <= one.x && two.y >= one.y) {
-            c1 += 1;
-        }
-
-        // Count how many ranges overlap at any point.
-        if (one.x <= two.x && one.y >= two.x) || (two.x <= one.x && two.y >= one.x) {
-            c2 += 1;
-        }
-    }
-
-    (c1, c2)
+            (c1, c2)
+        })
 }
 
 #[allow(dead_code)]
