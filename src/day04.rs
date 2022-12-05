7becefcebb1c1add141daed146482bcadbd36918
Juliette Lavoie <rie.juliette@gmail.com>
2022-12-04 19:42:29 -0500

    Refactored a bit more for Day 04.

--- src/day04.rs
+++ src/day04.rs
@@ -1,25 +1,7 @@
 // https://adventofcode.com/2022/day/4
 
-pub fn solve(data: &[String]) -> (i32, i32) {
-    // Iterate over each piece of data and convert it into ((String, String), (String, String)), using the split_line function.
-    let data: Vec<((i32, i32), (i32, i32))> = data
-        .iter()
-        .map(|x| crate::library::split_line(x, ",", "-"))
-        .map(|x| {
-            let first = x.0;
-            let second = x.1;
-            let first = (
-                first.0.parse::<i32>().unwrap(),
-                first.1.parse::<i32>().unwrap(),
-            );
-            let second = (
-                second.0.parse::<i32>().unwrap(),
-                second.1.parse::<i32>().unwrap(),
-            );
-            (first, second)
-        })
-        .collect();
-
+#[allow(clippy::type_complexity)]
+pub fn solve(data: &[((i32, i32), (i32, i32))]) -> (i32, i32) {
     // Process the data
     data.iter()
         .fold((0, 0), |(mut c1, mut c2), (first, second)| {
@@ -39,14 +21,33 @@ pub fn solve(data: &[String]) -> (i32, i32) {
         })
 }
 
+fn parse(data: &[String]) -> Vec<((i32, i32), (i32, i32))> {
+    data.iter()
+        .map(|x| crate::library::split_line(x, ",", "-"))
+        .map(|x| {
+            let first = x.0;
+            let second = x.1;
+            let first = (
+                first.0.parse::<i32>().unwrap(),
+                first.1.parse::<i32>().unwrap(),
+            );
+            let second = (
+                second.0.parse::<i32>().unwrap(),
+                second.1.parse::<i32>().unwrap(),
+            );
+            (first, second)
+        })
+        .collect()
+}
+
 #[allow(dead_code)]
 pub fn run() {
-    let res = solve(&crate::library::read_file("data/day04.txt"));
+    let res = solve(&parse(&crate::library::read_file("data/day04.txt")));
     println!("Day 04:\nStar 1: {}\nStar 2: {}\n", res.0, res.1);
 }
 
 #[allow(dead_code)]
 pub fn benchmark(c: &mut criterion::Criterion) {
-    let data = crate::library::read_file("data/day04.txt");
-    c.bench_function("Day 04", |b| b.iter(|| solve(&data)));
+    let data = &parse(&crate::library::read_file("data/day04.txt"));
+    c.bench_function("Day 04", |b| b.iter(|| solve(data)));
 }
