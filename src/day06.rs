bbfd4f1c89921b458907688cabcb7d91a0fdbde6
Juliette Lavoie <rie.juliette@gmail.com>
2022-12-06 10:06:50 -0500

    Added some code speedup to 06.

--- src/day06.rs
+++ src/day06.rs
@@ -1,47 +1,37 @@
 // https://adventofcode.com/2022/day/6
 
-use itertools::Itertools;
-
-pub fn solve(data: &[String]) -> (i32, i32) {
-    let s = &data[0];
-
-    let p1 = s
-        .chars()
-        .collect_vec()
+pub fn solve(data: &str) -> (i32, i32) {
+    let p1 = data
+        .as_bytes()
         .windows(4)
-        .take_while(|c| not_unique(c.iter().collect::<String>().as_str()))
+        .take_while(|c| not_unique(c))
         .count() as i32;
 
-    let p2 = s
-        .chars()
-        .collect_vec()
+    let p2 = data
+        .as_bytes()
         .windows(14)
-        .take_while(|c| not_unique(c.iter().collect::<String>().as_str()))
+        .take_while(|c| not_unique(c))
         .count() as i32;
 
     (p1 + 4, p2 + 14)
 }
 
-fn not_unique(rng: &str) -> bool {
-    let mut _mask = 0;
-    for c in rng.chars() {
-        _mask |= 1 << (c as u64 - 'A' as u64);
+fn not_unique(rng: &[u8]) -> bool {
+    let mut _mask: i32 = 0;
+    for c in rng {
+        _mask |= 1 << (*c - b'A');
     }
-    let mut set = std::collections::HashSet::new();
-    for c in rng.chars() {
-        set.insert(c);
-    }
-    set.len() != rng.len()
+    _mask.count_ones() != rng.len() as u32
 }
 
 #[allow(dead_code)]
 pub fn run() {
-    let res = solve(&crate::library::read_file("data/day06.txt"));
+    let res = solve(&crate::library::read_file("data/day06.txt")[0]);
     println!("Day 06:\nStar 1: {}\nStar 2: {}\n", res.0, res.1);
 }
 
 #[allow(dead_code)]
 pub fn benchmark(c: &mut criterion::Criterion) {
     let data = crate::library::read_file("data/day06.txt");
-    c.bench_function("Day 06", |b| b.iter(|| solve(&data)));
+    c.bench_function("Day 06", |b| b.iter(|| solve(&data[0])));
 }
