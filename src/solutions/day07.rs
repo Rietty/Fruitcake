1398e962ea8ae0f0be0aab3989670d483ca59173
Juliette H. Lavoie <35241556+Rietty@users.noreply.github.com>
2022-12-07 15:29:19 -0500

    Finished Day 07.

--- src/solutions/day07.rs
+++ src/solutions/day07.rs
@@ -1,7 +1,57 @@
 // https://adventofcode.com/2022/day/7
 
 pub fn solve(data: &[String]) -> (i32, i32) {
-    (0, 0)
+    // Create a vector to store the current directory-path we're in.
+    let mut path = Vec::new();
+
+    // Using a hashmap to map a path to a given size, initially I had a string but using a PathBuf provides more power and is easier to use.
+    let mut dir_sizes: HashMap<PathBuf, i32> = HashMap::new();
+
+    for line in data {
+        // Need to skip if our op is a lookup or a dir op.
+        if line.starts_with("dir") || line.starts_with("$ ls") {
+            continue;
+        }
+
+        // Tokenize our line.
+        let tokens: Vec<_> = line.split_whitespace().collect();
+
+        // Match for either the change directories, or on the file size + name. Can match an entire vector at once.
+        match tokens[..] { // Really cool syntax to match an array's parts all at once.
+            // If we leave a directory, pop that out of our path vector.
+            ["$", "cd", ".."] => {
+                path.pop();
+            } 
+
+            // Need to do next part second because if you put it before other it causes issues due to the capture variable resulting in unreached statements in the match expression.
+            ["$", "cd", name] => { // The random value can be captured into name variable. Kinda like a regex.
+                path.push(name); 
+            }
+
+            // If we have a file, that is a size and then file_name we can just add the size to the hash-map with the key as a collection of the path.
+            [size, _name] => { // Don't need _name, but use variable since it can be discarded and changed.
+                let size: i32 = size.parse().unwrap();
+                // For each path, that is for example we have something like dev/dir/a/b/c, we want to add the size at each step, so each directory always reflects the true size.
+                for i in 0..path.len() {
+                    let p = PathBuf::from_iter(&path[..=i]);
+                    *dir_sizes.entry(p).or_insert(0) += size; // So access entry, if it doesn't exist init it to 0. Then add the size to it.
+                }
+            }
+
+            _ => { // Do nothing, every case is accounted for.
+                println!("Merry Christmas!"); 
+            } 
+        };
+    }
+
+    // Sum of all directories under 100k. Need to look into how I can make the numbers easier to read.
+    let p1 = dir_sizes.clone().into_values().filter(|s| *s <= 100000).sum();
+
+    // Find and delete the smallest available directory that will result in the space needed.
+    let usable = 70000000 - dir_sizes.get(&PathBuf::from("/")).unwrap();
+    let p2 = dir_sizes.into_values().filter(|s| usable + s >= 30000000).min().unwrap();
+
+    (p1, p2)
 }
 
 #[allow(dead_code)]
