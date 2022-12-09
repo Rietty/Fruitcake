7cb866b6224c2d13adf2aba47cfd688106558fe0
Juliette Lavoie <rie.juliette@gmail.com>
2022-12-08 22:08:38 -0500

    Updated Day 08.

--- src/solutions/day08.rs
+++ src/solutions/day08.rs
@@ -1,91 +1,79 @@
 // https://adventofcode.com/2022/day/8
 
 pub fn solve(data: &Vec<Vec<i32>>) -> (i32, i32) {
-    let mut p1: i32 = 0;
-    let mut p2: i32 = 0;
+    // Grab the size of the tree vector.
+    let rows = data.len();
+    let cols = data[0].len();
 
-    for (i, r) in data.iter().enumerate() {
-        for (j, &tree) in r.iter().enumerate() {
-            let mut visible_left = true;
-            let mut visible_right = true;
-            let mut visible_top = true;
-            let mut visible_bottom = true;
+    // Create a 2D vector of size rows x cols filled with 0s.
+    let mut outside_vis = vec![vec![0; cols]; rows];
+    let mut scenic_vis = vec![vec![1; cols]; rows];
 
-            for k in 0..j {
-                if data[i][k] >= tree {
-                    visible_left = false;
-                    break;
-                }
-            }
-            
-            for k in (j + 1)..r.len() {
-                if data[i][k] >= tree {
-                    visible_right = false;
-                    break;
-                }
+    // Next we need a simple way to check if the given coordinates are valid, can use a closure for this, and ensure they are in range.
+    let is_valid = |x: i32, y: i32| x < rows as i32 && y < cols as i32 && x >= 0 && y >= 0;
+
+    // Create a closure called process that will run a bunch of steps and process the data.
+    let mut process_outside = |mut x: usize, mut y: usize, factor: (i32, i32)| {
+        let mut max: i32 = -1; // Setting this to -1 will ensure that the first value is always greater than it, especially if it is a 0.
+
+        while is_valid(x as i32, y as i32) {
+            if data[x][y] > max {
+                outside_vis[x][y] = 1;
+                max = data[x][y];
             }
 
-            for k in 0..i {
-                if data[k][j] >= tree {
-                    visible_top = false;
-                    break;
-                }
-            }
-            
-            for k in (i + 1)..data.len() {
-                if data[k][j] >= tree {
-                    visible_bottom = false;
-                    break;
-                }
-            }
-
-            // If the tree is visible from any direction, increment the counter
-            if visible_left || visible_right || visible_top || visible_bottom {
-                p1 += 1;
-            }
+            x += factor.0 as usize;
+            y += factor.1 as usize;
         }
+    };
+
+    let mut process_scenic = |mut x: usize, mut y: usize, factor: (i32, i32)| {
+        // Create an index variable to keep track of stuff for stack.
+        let mut index = 0;
+
+        // Create a stack by using a vector that contains a tuple with initial value of (10, 0).
+        let mut stack = vec![(10, 0)];
+
+        // While we have a valid index..
+        while is_valid(x as i32, y as i32) {
+            // While stack's last element's first value is less than the value at the current index...
+            while stack.last().unwrap().0 < data[x][y] {
+                // Pop the last element off the stack.
+                stack.pop();
+            }
+
+            // Update the visibility of the current index by multiplying out with the index minus the stack's last element's second value.
+            scenic_vis[x][y] *= index - stack.last().unwrap().1;
+            // Add to the stack the current value and the current index.
+            stack.push((data[x][y], index));
+
+            // Increment the x, y by the factor.
+            x += factor.0 as usize;
+            y += factor.1 as usize;
+            // Increment the index.
+            index += 1;
+        }
+    };
+
+    // For the range from 0 to rows... call the process closure with the current row and the current column, and a factor of 0, 1.
+    for i in 0..rows {
+        process_outside(i, 0, (0, 1));
+        process_outside(i, cols - 1, (0, -1));
+        process_scenic(i, 0, (0, 1));
+        process_scenic(i, cols - 1, (0, -1));
     }
 
-    for row in 0..data.len() {
-        for col in 0..data[row].len() {
-            let curr_height = data[row][col];
-
-            let mut left: i32 = 0;
-            let mut right: i32 = 0;
-            let mut top: i32 = 0;
-            let mut bottom: i32 = 0;
-
-            for k in (0..row).rev() {
-                top += 1;
-                if data[k][col] >= curr_height {
-                    break;
-                }
-            }
-
-            for k in (0..col).rev() {
-                left += 1;
-                if data[row][k] >= curr_height {
-                    break;
-                }
-            }
-
-            for k in (row + 1)..data.len() {
-                bottom += 1;
-                if data[k][col] >= curr_height {
-                    break;
-                }
-            }
-
-            for k in (col + 1)..data[row].len() {
-                right += 1;
-                if data[row][k] >= curr_height {
-                    break;
-                }
-            }
-
-            p2 = std::cmp::max(p2, left * right * top * bottom);
-        }
+    // For the range from 0 to cols... call the process closure with the current row and the current column, and a factor of 1, 0.
+    for i in 0..cols {
+        process_outside(0, i, (1, 0));
+        process_outside(rows - 1, i, (-1, 0));
+        process_scenic(0, i, (1, 0));
+        process_scenic(rows - 1, i, (-1, 0));
     }
+
+    // Sum the outside visibility by flattening the vector and summing the values.
+    let p1 = outside_vis.iter().flatten().sum();
+    let p2 = *scenic_vis.iter().flatten().max().unwrap();
 
     (p1, p2)
 }
