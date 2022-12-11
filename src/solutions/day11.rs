e4ecc66bcf0a4c9b17b602380fc883ac9bf1ba12
Juliette Lavoie <rie.juliette@gmail.com>
2022-12-11 12:03:35 -0500

    Added a parser for Day 11.

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -29,7 +29,7 @@ impl Monkey {
                 op2: String::new(),
                 oper: ' ',
             },
-            test: Vec::new(),
+            test: vec![0, 0, 0],
         }
     }
 }
@@ -39,7 +39,7 @@ pub fn solve(monkeys: &mut Vec<Monkey>) -> (i64, i64) {
     let p1 = calculate(&mut p1_monkeys, 20, String::from("1"));
 
     let mut p2_monkeys = monkeys.to_owned();
-    let p2 = calculate(&mut p2_monkeys, 10000, String::from("2"));
+    let p2 = calculate(&mut p2_monkeys, 10_000, String::from("2"));
 
     (p1, p2)
 }
@@ -56,6 +56,13 @@ pub fn calculate(monkeys: &mut Vec<Monkey>, iterations: i64, part: String) -> i6
 
     // Vec to store the inspected counts of each monkey, size is of the monkeys vec, default value is 0.
     let mut inspected = vec![0; monkeys.len()];
+
+    // Part 2, we set the new worry to modulus of the product of all the values at test[0] for all monkeys.
+    let mut product = 1;
+    // Use an iterator to get the product of all the values at test[0].
+    for m in monkeys.iter() {
+        product *= m.test[0];
+    }
 
     // Need to do iterations times.
     for _ in 0..iterations {
@@ -87,13 +94,6 @@ pub fn calculate(monkeys: &mut Vec<Monkey>, iterations: i64, part: String) -> i6
                     }
 
                     "2" => {
-                        // Part 2, we set the new worry to modulus of the product of all the values at test[0] for all monkeys.
-                        let mut product = 1;
-                        // Use an iterator to get the product of all the values at test[0].
-                        for m in monkeys.iter() {
-                            product *= m.test[0];
-                        }
-
                         new_worry %= product;
                     }
 
@@ -117,87 +117,83 @@ pub fn calculate(monkeys: &mut Vec<Monkey>, iterations: i64, part: String) -> i6
 
     // Sort the inspected vector.
     inspected.sort();
-    inspected.reverse();
 
     // Return the product of the highest and 2nd highest values.
-    inspected[0] * inspected[1]
+    inspected[inspected.len() - 1] * inspected[inspected.len() - 2]
 }
 
-pub fn parse(_data: &[String]) -> Vec<Monkey> {
-    let monkeys = vec![
-        Monkey {
-            worries: vec![78, 53, 89, 51, 52, 59, 58, 85],
-            operation: Operation {
-                op1: String::from("old"),
-                op2: String::from("3"),
-                oper: '*',
-            },
-            test: vec![5, 2, 7],
-        },
-        Monkey {
-            worries: vec![64],
-            operation: Operation {
-                op1: String::from("old"),
-                op2: String::from("7"),
-                oper: '+',
-            },
-            test: vec![2, 3, 6],
-        },
-        Monkey {
-            worries: vec![71, 93, 65, 82],
-            operation: Operation {
-                op1: String::from("old"),
-                op2: String::from("5"),
-                oper: '+',
-            },
-            test: vec![13, 5, 4],
-        },
-        Monkey {
-            worries: vec![67, 73, 95, 75, 56, 74],
-            operation: Operation {
-                op1: String::from("old"),
-                op2: String::from("8"),
-                oper: '+',
-            },
-            test: vec![19, 6, 0],
-        },
-        Monkey {
-            worries: vec![85, 91, 90],
-            operation: Operation {
-                op1: String::from("old"),
-                op2: String::from("4"),
-                oper: '+',
-            },
-            test: vec![11, 3, 1],
-        },
-        Monkey {
-            worries: vec![67, 96, 69, 55, 70, 83, 62],
-            operation: Operation {
-                op1: String::from("old"),
-                op2: String::from("2"),
-                oper: '*',
-            },
-            test: vec![3, 4, 1],
-        },
-        Monkey {
-            worries: vec![53, 86, 98, 70, 64],
-            operation: Operation {
-                op1: String::from("old"),
-                op2: String::from("6"),
-                oper: '+',
-            },
-            test: vec![7, 7, 0],
-        },
-        Monkey {
-            worries: vec![88, 64],
-            operation: Operation {
-                op1: String::from("old"),
-                op2: String::from("old"),
-                oper: '*',
-            },
-            test: vec![17, 2, 5],
-        },
-    ];
+pub fn parse(data: &[String]) -> Vec<Monkey> {
+    // Parser will parse the data into a vector of monkeys.
+    let mut monkeys: Vec<Monkey> = Vec::new();
+
+    // Regex to parse the data.
+    let re_monkey = Regex::new(r"Monkey (\d+):").unwrap();
+    let re_starting_items = Regex::new(r"Starting items: (\d+(, \d+)*)").unwrap();
+    let re_operation = Regex::new(r"Operation: (\w+) = (\w+) ([\*\+]) (\w+)").unwrap();
+    let re_test = Regex::new(r"Test: divisible by (\d+)").unwrap();
+    let re_if_true = Regex::new(r"If true: throw to monkey (\d+)").unwrap();
+    let re_if_false = Regex::new(r"If false: throw to monkey (\d+)").unwrap();
+
+    for line in data {
+        // If line is empty, skip it.
+        if line.is_empty() {
+            continue;
+        }
+
+        // If the line matches re_monkey, we create a new monkey.
+        if re_monkey.is_match(line) {
+            monkeys.push(Monkey::new());
+        }
+
+        // If the line matches re_starting_items, we parse the starting items, and add them to the worries of the last monkey.
+        if re_starting_items.is_match(line) {
+            let mut worries = Vec::new();
+            for cap in re_starting_items.captures_iter(line) {
+                for item in cap[1].split(", ") {
+                    worries.push(item.parse::<i64>().unwrap());
+                }
+            }
+
+            let last_monkey = monkeys.len() - 1;
+            monkeys[last_monkey].worries = worries;
+        }
+
+        // If the line matches re_operation, we parse the operation.
+        if re_operation.is_match(line) {
+            let last_monkey = monkeys.len() - 1;
+            for cap in re_operation.captures_iter(line) {
+                monkeys[last_monkey].operation = Operation {
+                    op1: cap[2].to_string(),
+                    op2: cap[4].to_string(),
+                    oper: cap[3].chars().next().unwrap(),
+                };
+            }
+        }
+
+        // If the line matches re_test, we parse the test.
+        if re_test.is_match(line) {
+            let last_monkey = monkeys.len() - 1;
+            for cap in re_test.captures_iter(line) {
+                monkeys[last_monkey].test[0] = cap[1].parse::<i64>().unwrap();
+            }
+        }
+
+        // If the line matches re_if_true, we parse the if true.
+        if re_if_true.is_match(line) {
+            let last_monkey = monkeys.len() - 1;
+            for cap in re_if_true.captures_iter(line) {
+                monkeys[last_monkey].test[1] = cap[1].parse::<i64>().unwrap();
+            }
+        }
+
+        // If the line matches re_if_false, we parse the if false.
+        if re_if_false.is_match(line) {
+            let last_monkey = monkeys.len() - 1;
+            for cap in re_if_false.captures_iter(line) {
+                monkeys[last_monkey].test[2] = cap[1].parse::<i64>().unwrap();
+            }
+        }
+    }
 
     monkeys
 }
@@ -222,12 +218,12 @@ mod tests {
     #[test]
     fn part1() {
         let res = solve(&mut parse(&crate::library::read_file("testdata/day11.txt")));
-        assert_eq!(res.0, 0);
+        assert_eq!(res.0, 10605);
     }
 
     #[test]
     fn part2() {
         let res = solve(&mut parse(&crate::library::read_file("testdata/day11.txt")));
-        assert_eq!(res.1, 0);
+        assert_eq!(res.1, 2713310158);
     }
 }
