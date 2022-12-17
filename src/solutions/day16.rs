be244d24a183c3c20a7d8b4990713ebd574de085
Juliette Lavoie <rie.juliette@gmail.com>
2022-12-16 19:27:38 -0500

    Really fucking annoying Stack Overflow.

--- src/solutions/day16.rs
+++ src/solutions/day16.rs
@@ -1,13 +1,38 @@
 // https://adventofcode.com/2022/day/16
 
-use std::collections::HashMap;
-use std::collections::HashSet;
+use std::{
+    collections::{HashMap, HashSet},
+    hash::{Hash, Hasher},
+};
 
 // Create a structure to hold the data for each room. The flow-rate, and the exits it has via tunnels. Since it'll be used in a HashMap, we don't need to store ID, as that'll be the key.
 #[derive(Debug)]
 pub struct Room {
     flow: i32,
     exits: Vec<String>,
+}
+
+// Shamelessly stolen from: https://stackoverflow.com/questions/27828487/is-it-possible-to-use-a-hashset-as-the-key-to-a-hashmap
+pub struct Wrapper<T>(HashSet<T>);
+
+impl<T> PartialEq for Wrapper<T>
+where
+    T: Eq + Hash,
+{
+    fn eq(&self, other: &Wrapper<T>) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl<T> Eq for Wrapper<T> where T: Eq + Hash {}
+
+impl<T> Hash for Wrapper<T> {
+    fn hash<H>(&self, _state: &mut H)
+    where
+        H: Hasher,
+    {
+        // We don't care about the hash value, we just want to use the set as a key.
+    }
 }
 
 pub fn solve(data: &HashMap<String, Room>) -> (i32, i32) {
@@ -68,12 +93,149 @@ pub fn solve(data: &HashMap<String, Room>) -> (i32, i32) {
     // This part is a basic DFS.
     let seen: HashSet<String> = HashSet::new();
     let targets: HashSet<String> = good_rooms.keys().map(|x| x.to_string()).collect();
-
     let p1 = best_total_flow("AA", 30, &seen, targets, data, &distances);
+
+    // Part 2.
+    // Create a map that will hold a HashSet as a key, and the maximum-flow as the value given that initial subset.
+    let seen: HashSet<String> = HashSet::new();
+    let mut endroom_flowrates: HashMap<Wrapper<String>, i32> = HashMap::new();
+
+    // Call the best_endroom_total_flow function.
+    best_endroom_total_flow(
+        "AA",
+        26,
+        &seen,
+        0,
+        &good_rooms,
+        &distances,
+        &mut endroom_flowrates,
+        data,
+    );
+
+    // Print size of endroom_flowrates map.
+    println!("Size of endroom_flowrates map: {}", endroom_flowrates.len());
+
+    // Call the fill_missing_rooms function.
+    fill_missing_rooms(&good_rooms, &mut endroom_flowrates);
+
+    // Print size of endroom_flowrates map.
+    println!("Size of endroom_flowrates map: {}", endroom_flowrates.len());
+
+    // Print the endroom_flowrates map by using an iterator to print the set as a string, and the flow-rate as an integer.
+    /*
+    for (k, v) in endroom_flowrates.iter() {
+        println!(
+            "{:?} -> {}",
+            k.0.iter().map(|x| x.to_string()).collect::<Vec<String>>(),
+            v
+        );
+    }
+     */
 
     (p1, 0)
 }
 
+// Function designed to cover any missing rooms in the endroom_flowrates map, that way we can easily compute part 2.
+pub fn fill_missing_rooms(
+    good_rooms: &HashMap<&String, &Room>,
+    endroom_flowrates: &mut HashMap<Wrapper<String>, i32>,
+) -> i32 {
+    // Set current to be the a set in which it is all the rooms that are in the good_rooms map, minus "AA".
+    let current: HashSet<String> = good_rooms
+        .keys()
+        .map(|x| x.to_string())
+        .filter(|x| x != "AA")
+        .collect();
+
+    // If that set is not present in the endroom_flowrates map, calculate the best flow-rate for that set and add it to the map.
+    if !endroom_flowrates.contains_key(&Wrapper(current.clone())) {
+        let mut best_flow = 0;
+        for room in &current {
+            // Create a subset that is the current set minus the current room.
+            let mut subset = current.clone();
+            subset.remove(room);
+
+            // Get the flow-rate for the subset using a recursive call to the fill_missing_rooms function.
+            let flow = fill_missing_rooms(good_rooms, endroom_flowrates);
+
+            // If the flow-rate is greater than the best flow-rate, set the best flow-rate to be the flow-rate.
+            best_flow = std::cmp::max(best_flow, flow);
+        }
+
+        // Set the best flow-rate for the current set to be the best flow-rate.
+        endroom_flowrates.insert(Wrapper(current.clone()), best_flow);
+    }
+
+    // Return the best flow-rate for the current set.
+    endroom_flowrates[&Wrapper(current)]
+}
+
+// Function that will get the best-flow rate as well as record the best flow-rate for each subset of rooms.
+#[allow(clippy::too_many_arguments)]
+pub fn best_endroom_total_flow(
+    current: &str,
+    time: i32,
+    seen: &HashSet<String>,
+    current_flow: i32,
+    good_rooms: &HashMap<&String, &Room>,
+    distances: &HashMap<(String, String), i32>,
+    endroom_flowrates: &mut HashMap<Wrapper<String>, i32>,
+    rooms: &HashMap<String, Room>,
+) -> i32 {
+    // Create a copy of the seen set.
+    let mut seen = seen.clone();
+
+    // Add the current room to the seen set.
+    seen.insert(current.to_string());
+
+    // Create a hashset called targets and put a copy of each key in the good_rooms map into it.
+    let mut targets: HashSet<String> = good_rooms.keys().map(|x| x.to_string()).collect();
+    // Remove anything that is in the seen set from the targets set.
+    targets = targets.difference(&seen).map(|x| x.to_string()).collect();
+
+    // Create a new set which will be used as key by taking the seen set and removing/filtering out the "AA" entry. If this set is not in the endroom_flowrates map, add it with the current flow-rate.
+    // If it exists, check if the current flow-rate is greater than the one in the map, if it is, replace it.
+    let mut key = seen.clone();
+    key.remove("AA");
+
+    endroom_flowrates
+        .entry(Wrapper(key))
+        .and_modify(|e| {
+            if *e < current_flow {
+                *e = current_flow;
+            }
+        })
+        .or_insert(current_flow);
+
+    let mut best_flow = 0;
+
+    for t in &targets {
+        let time_left = time - distances[&(current.to_string(), t.to_string())] - 1;
+
+        // If the time left is more than 0, we can continue.
+        if time_left > 0 {
+            let mut new_flow = time_left * rooms[&t.to_string()].flow;
+
+            new_flow += best_endroom_total_flow(
+                t,
+                time_left,
+                &seen,
+                current_flow + new_flow,
+                good_rooms,
+                distances,
+                endroom_flowrates,
+                rooms,
+            );
+
+            // Set the best_flow to max of the current flow and the best flow.
+            best_flow = std::cmp::max(best_flow, new_flow);
+        }
+    }
+
+    best_flow
+}
+
+// Just a DFS search that gets the best flow-rate over all possible paths and returns it.
 pub fn best_total_flow(
     current: &str,
     time: i32,
